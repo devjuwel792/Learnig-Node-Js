47758c326b2f693e7d6a0f631b9732fa50f0bb17
devjuwel792 <developerjuwel792@gmail.com>
2025-12-01 17:12:50 +0600

    `Added express.json() and express.urlencoded() middleware and new routes for request handling`

--- Project_1/index.js
+++ Project_1/index.js
@@ -5,6 +5,8 @@ app.listen(5000, () => {
   console.log(`Successfully Connected Port : 5000 `);
 });
 app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   const user = [
@@ -62,4 +64,24 @@ app.get("/end", (req, res) => {
 app.get("/status", (req, res) => {
   res.set("custom-header", "hello123");
   res.send(res.get("custom-header"));
+});
+
+app.get("/request", (req, res) => {
+  //   if (req.accepts("html")) {
+  //     res.send(req.header);
+  //   } else if (req.accepts("json")) {
+  //     res.send({ message: "Hello JSON" });
+  //   } else if (req.accepts("xml")) {
+  //     res.send("<message>Hello XML</message>");
+  //   } else {
+  //     res.send("Content Type NOt Supp")
+  //   }
+
+  if (req.is("application/json")) {
+    res.send("Valid json data");
+  } else if (req.is("text/html")) {
+    res.send("HTML Data");
+  } else {
+    res.send("not support any formate");
+  }
 });
