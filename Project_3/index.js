0ab4fb8cb8e0a3486cab6c96d2adecf0d56de139
devjuwel792 <developerjuwel792@gmail.com>
2025-12-02 14:34:12 +0600

    `Updated Express app and EJS templates for Project 3`

--- Project_3/index.js
+++ Project_3/index.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 // Middleware
 
-app.set("view engine",'ejs')
-app.use(express.urlencoded({extended:false}))
-app.use(express.static("public"))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 //Routes
-app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/show-content", (req, res) => res.send("Hello World!"));
-app.get("/add-content", (req, res) => res.send("Hello World!"));
-app.post("/add-content", (req, res) => res.send("Hello World!"));
-app.get("/update-content", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/show-content", (req, res) => {
+  res.render("show-content");
+});
+app.get("/add-content", (req, res) => {
+  res.render("add-content");
+});
+app.post("/add-content", (req, res) => {
+  
+});
+app.get("/update-content", (req, res) => {
+  res.render("update-content");
+});
 app.post("/update-content", (req, res) => res.send("Hello World!"));
 app.post("/delete-content", (req, res) => res.send("Hello World!"));
