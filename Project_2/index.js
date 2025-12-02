511fb827a8cc80b41c722d0457c8fefc51a19a15
devjuwel792 <developerjuwel792@gmail.com>
2025-12-02 13:51:03 +0600

    `Added express middleware for URL encoding and static file serving`

--- Project_2/index.js
+++ Project_2/index.js
@@ -5,6 +5,8 @@ const port = 5000;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.get("/", (req, res) => {
@@ -17,4 +19,12 @@ app.get("/about-us", (req, res) => {
     message: "",
     items,
   });
+});
+
+app.get("/form", (req, res) => {
+  res.render("form", { message: null });
+});
+app.post("/submit", (req, res) => {
+  const message = `Hello Your Email Is : ${req.body.email} and Password is ${req.body.password}`;
+  res.render("form", { message });
 });
