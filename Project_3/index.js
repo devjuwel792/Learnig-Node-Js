e2429c7b6f6976b51d100d1b2b00f593f4cea9aa
devjuwel792 <developerjuwel792@gmail.com>
2025-12-02 16:37:54 +0600

    `Added mongoose and database connection, updated routes and views to use mongoose and MongoDB`

--- Project_3/index.js
+++ Project_3/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Contact = require("./models/contacts.model.js");
+
 const app = express();
 const port = 5000;
+
+// Database Connection
+mongoose.connect("mongodb://127.0.0.1:27017/contacts-crud").then(() => {
+  console.log("Database Connected.");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 // Middleware
@@ -9,20 +17,29 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 //Routes
-app.get("/", (req, res) => {
-  res.render("home");
+
+// GET
+app.get("/", async (req, res) => {
+  const data = await Contact.find();
+  // res.json(data)
+  res.render("home", { data });
 });
-app.get("/show-content", (req, res) => {
+
+app.get("/show-content/:id", (req, res) => {
   res.render("show-content");
 });
+
 app.get("/add-content", (req, res) => {
   res.render("add-content");
 });
-app.post("/add-content", (req, res) => {
-  
-});
-app.get("/update-content", (req, res) => {
+
+app.get("/update-content/:id", (req, res) => {
   res.render("update-content");
 });
-app.post("/update-content", (req, res) => res.send("Hello World!"));
-app.post("/delete-content", (req, res) => res.send("Hello World!"));
+
+// POST
+app.post("/add-content", (req, res) => {});
+
+app.post("/update-content", (req, res) => {});
+
+app.post("/delete-content/:id", (req, res) => {});
