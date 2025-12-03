9be0a4a30a14732354af0c5881de45a882223cd3
devjuwel792 <developerjuwel792@gmail.com>
2025-12-03 16:43:53 +0600

    `Added mongoose import and validation for contact IDs in various controller functions`

--- Project_4_MVC/controllers/contacts.controller.js
+++ Project_4_MVC/controllers/contacts.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Contact from "../models/contacts.model.js";
 
 export const getContacts = async (req, res) => {
@@ -8,6 +9,13 @@ export const getContacts = async (req, res) => {
 
 export const showContacts = async (req, res) => {
   // const data = await Contact.findOne({ _id: req.params.id });
+
+  let paramId = mongoose.Types.ObjectId.isValid(req.params.id);
+  if (!paramId) {
+    res.render("404", {
+      message: "The page you are looking for does not exist.",
+    });
+  }
   const data = await Contact.findById(req.params.id);
   //  res.json(data);
   res.render("show-content", { data });
@@ -18,6 +26,13 @@ export const addContactPage = (req, res) => {
 };
 
 export const updateContactPage = async (req, res) => {
+  let paramId = mongoose.Types.ObjectId.isValid(req.params.id);
+  if (!paramId) {
+    res.render("404", {
+      message: "The page you are looking for does not exist.",
+    });
+  }
+
   const data = await Contact.findById(req.params.id);
   res.render("update-content", { data });
 };
@@ -35,6 +50,12 @@ export const addContact = async (req, res) => {
 };
 
 export const updateContact = async (req, res) => {
+  let paramId = mongoose.Types.ObjectId.isValid(req.params.id);
+  if (!paramId) {
+    res.render("404", {
+      message: "The page you are looking for does not exist.",
+    });
+  }
   await Contact.findByIdAndUpdate(req.params.id, {
     first_name: req.body.first_name,
     last_name: req.body.last_name,
@@ -46,9 +67,12 @@ export const updateContact = async (req, res) => {
 };
 
 export const deleteContact = async (req, res) => {
+  let paramId = mongoose.Types.ObjectId.isValid(req.params.id);
+  if (!paramId) {
+    res.render("404", {
+      message: "The page you are looking for does not exist.",
+    });
+  }
   await Contact.findByIdAndDelete(req.params.id);
   res.redirect("/");
 };
-
-
-
